a05ba983e47b504d70fdad65047d6d3d6fc7d8b2
lukasz <lukasz.ciswicki@gmail.com>
2024-12-13 10:04:59 +0100

    lexer

---
+++ compiler/lexer.py
@@ -0,0 +1,49 @@
+from sly import Lexer
+
+class MyLexer(Lexer):
+    
+    tokens = {PROCEDURE, IS, BEGIN, END, PROGRAM, IF, THEN, ELSE, ENDIF,
+              WHILE, DO, ENDWHILE, REPEAT, UNTIL, FOR, FROM, TO, ENDFOR,
+              READ, WRITE, PID, NUM, T}
+    
+    literals = {'+', '-', '*', '/', '%', '=', '!=', '>', '<', '>=', '<=',
+                '(', ')', '[', ']', ',', ';', ':', ':='}
+    
+    PROCEDURE = r'PROCEDURE'
+    IS = r'IS'
+    BEGIN = r'BEGIN'
+    END = r'END'
+    PROGRAM = r'PROGRAM'
+    IF = r'IF'
+    THEN = r'THEN'
+    ELSE = r'ELSE'
+    ENDIF = r'ENDIF'
+    WHILE = r'WHILE'
+    DO = r'DO'
+    ENDWHILE = r'ENDWHILE'
+    REPEAT =r'REPEAT'
+    UNTIL =r'UNTIL'
+    FOR = r'FOR'
+    FROM = r'FROM'
+    TO = r'TO'
+    ENDFOR = r'ENDFOR'
+    READ = r'READ'
+    WRITE = r'WRITE'
+    T = r'T'    
+    PID = r'[_a-z]+'
+    NUM = r'0|[1-9]+[0-9]*'
+    
+    ignore = ' \t'
+    
+    ignore_comment = r'\#.*'
+    
+    ignore_newline = r'\n+'
+    
+    
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += len(t.value)
+        
+    def error(self, t):
+        print("Line %d: Wrong character %r '%s'" % (self.lineno, t.value[0]))
+        self.index += 1
