d3b888932eecfa99d0a1be4b92f94a22379fc9fb
lukasz <lukasz.ciswicki@gmail.com>
2024-12-16 20:52:56 +0100

    SymbolTable, CodeGenerator: constructor, get_declarations(), generate_code() start

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ----------------------------- Author: Łukasz Ciświcki
 
-Program compiles programs written in a custom language specified by a grammar to a version of assembly.
+Program compiles programs written in a custom language specified by a grammar to a version of assembly emulated by a virtual machine.
 
 ----------------------------- FILES
 
--- compiler/generator.py
+++ compiler/generator.py
@@ -1,4 +1,4 @@
-from sly import Parser
+from parser import MyParser
 from lexer import MyLexer
 
 
@@ -42,16 +42,79 @@ class SymbolTable:
         
         
         
+class CodeGenerator:
+    def __init__(self, program, debug=False):
+        self.program = program
+        self.debug = debug
+        self.procedures = program[1]
+        self.main = program[2]
         
-if __name__ == '__main__':
-    st = SymbolTable()
-    st.add_symbol('n1')
-    st.add_symbol('n2')
+        if self.debug:
+            print("\nprocedures: ", self.procedures)
+            print("\nmain: ", self.main)
+        
+        
     
-    n = st.get_symbol('n1')
+    def generate_code(self):
+        # declarations will be 
+        declarations = self.get_declarations()
+    
+    
+    
+    def get_declarations(self):
+        """ Gets declarations from program
+        and returns them in the recursive form """
+        
+        if self.debug:
+            print("\nmain[0]: ", self.main[0])
+            
+        if self.main[0] != 'mn=LONG': # program has no declarations
+            if self.debug:
+                    print("Program has no declarations")
+            return None
+        
+        declarations = self.main[1]    
+        if self.debug:
+            print("\ndeclarations: ", declarations)
+            
+        return declarations
+    
+    
+    # def extract_decs(self, decs):
+    #     """ Changes declarations from
+    #     recursive form to a list """
+        
+        
+        
 
-    st.display()
-    st.find_name(1)
     
     
-    # item = table['nazwa']
+    def group_commands(self, commands):
+        comm_list = []
+        comm_list.append(commands[1]) 
+        if commands[0] == 'comms=SINGLE':        
+            return comm_list
+        
+        else:
+            comm_list.append(self.group_commands(commands[1]))
+            return comm_list
+
+
+
+
+if __name__ == '__main__':
+    lexer = MyLexer()
+    parser = MyParser()
+
+    with open('examples/program0.imp', 'r') as file:
+        data = file.read()
+        
+    tokens = lexer.tokenize(data)
+    
+    parsed = parser.parse(tokens)
+    
+    
+    gen = CodeGenerator(parsed, True)
+    gen.get_declarations()
+    
+    # print(result)
