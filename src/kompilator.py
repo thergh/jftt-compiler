ba295401320d5d0ee3cf8f83e15279c02c17ccd8
lukasz <lukasz.ciswicki@gmail.com>
2024-12-31 13:33:58 +0100

    kompilator changes

--- src/kompilator.py
+++ src/kompilator.py
@@ -4,19 +4,13 @@ from generator import CodeGenerator
 import sys
 
 
+
 if __name__ == '__main__':
     lexer = MyLexer()
     parser = MyParser()
-    
-    # input = 'examples/my0.imp'
-    # input = 'examples/program1.imp'
-    # output = 'output/my-out.mr'
 
     input = sys.argv[1]
-    
     output = sys.argv[2]
-    
-
 
     with open(input, 'r') as file:
         data = file.read()
@@ -27,7 +21,6 @@ if __name__ == '__main__':
     
     gen = CodeGenerator(parsed, True)
     
-    # print(gen.code_list_to_string())
     code = gen.generate_code()
 
     with open(output, 'w') as file:
